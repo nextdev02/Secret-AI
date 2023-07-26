9d4b18216a6fc7f41b5bf15ba98d44207151dfc2
nextdev2 <nextdev0.02@gmail.com>
2023-07-26 11:10:04 +0600

    issue solved

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Open AI',
+  title: 'Secret AI',
   description: 'This website are working like Artificial Inteligence',
 }
 
