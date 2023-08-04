13ac0f32740987da7f8da97dfcfce088f74bf968
nextdev2 <nextdev0.02@gmail.com>
2023-08-05 05:03:51 +0600

    Home Page Update

--- components/loader.tsx
+++ components/loader.tsx
@@ -11,7 +11,7 @@ export const Loader = () => {
         />
       </div> */}
       <p className="text-sm text-muted-foreground">
-        SecretAI is running...
+        Secret AI is running...
       </p>
     </div>
   );
