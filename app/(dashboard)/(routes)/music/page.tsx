9bd2fbc3dc0647e301070ced26e6fc5c3e131b23
nextdev2 <nextdev0.02@gmail.com>
2023-08-06 10:44:37 +0600

    music error

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -105,7 +105,7 @@ const MusicPage = () => {
           </div>
         )}
         {!music && !isLoading && (
-          <Empty label="No music generated." />
+          <Empty label="Music Genarate is not working" />
         )}
         {music && (
           <audio controls className="w-full mt-8">
