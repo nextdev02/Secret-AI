6f64383061770c934b4c9690cd41ff3e309659c6
nextdev2 <nextdev0.02@gmail.com>
2023-08-06 10:50:42 +0600

    video route

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,13 +1,6 @@
-import Replicate from "replicate";
+
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-
-// import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
-// import { checkSubscription } from "@/lib/subscription";
-
-const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN!,
-});
 
 export async function POST(
   req: Request
@@ -25,27 +18,6 @@ export async function POST(
       return new NextResponse("Prompt is required", { status: 400 });
     }
 
-    // const freeTrial = await checkApiLimit();
-    // const isPro = await checkSubscription();
-
-    // if (!freeTrial && !isPro) {
-    //   return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
-    // }
-
-    const response = await replicate.run(
-      "anotherjesse/zeroscope-v2-xl:71996d331e8ede8ef7bd76eba9fae076d31792e4ddf4ad057779b443d6aea62f",
-      {
-        input: {
-          prompt,
-        }
-      }
-    );
-
-    // if (!isPro) {
-    //   await incrementApiLimit();
-    // }
-
-    return NextResponse.json(response);
   } catch (error) {
     console.log('[VIDEO_ERROR]', error);
     return new NextResponse("Internal Error", { status: 500 });
