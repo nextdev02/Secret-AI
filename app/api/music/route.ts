da252c89762862c57a71bd482a43bfd5722cdccf
nextdev2 <nextdev0.02@gmail.com>
2023-08-06 10:40:37 +0600

    next

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -2,8 +2,6 @@ import Replicate from "replicate";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
-// import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
-// import { checkSubscription } from "@/lib/subscription";
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!,
@@ -25,12 +23,7 @@ export async function POST(
       return new NextResponse("Prompt is required", { status: 400 });
     }
 
-    // const freeTrial = await checkApiLimit();
-    // const isPro = await checkSubscription();
 
-    // if (!freeTrial && !isPro) {
-    //   return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
-    // }
 
     const response = await replicate.run(
       "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
@@ -41,10 +34,7 @@ export async function POST(
       }
     );
 
-    // if (!isPro) {
-    //   await incrementApiLimit();
-    // }
-
+    
     return NextResponse.json(response);
   } catch (error) {
     console.log('[MUSIC_ERROR]', error);
